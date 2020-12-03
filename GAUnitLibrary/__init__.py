1f4fca56dc36034b90c902247aa960ea462c0628
VinceCabs <vctcbns@gmail.com>
2020-12-03 23:34:52 +0100

    can check webdriver Performance Logs now
    - upgrade to GAUnit 1.1.5
    - add `check tracking from perf log` keyword
    - add sample
    - add documentation

--- GAUnitLibrary/__init__.py
+++ GAUnitLibrary/__init__.py
@@ -1,8 +1,15 @@
-from .__about__ import __version__
 import gaunit
+from robot.libraries.BuiltIn import BuiltIn
+
+from .__about__ import __version__
 
 
 class GAUnitLibrary:
+    """GAUnitLibrary is a Robot Framework library to interface with
+    [https://pypi.org/project/gaunit/|GAUnit]
+
+    See [https://github.com/VinceCabs/robotframework-gaunitlibrary|project page] for more info
+    """
 
     ROBOT_LIBRARY_VERSION = __version__
     ROBOT_LIBRARY_SCOPE = "GLOBAL"
@@ -12,13 +19,44 @@ class GAUnitLibrary:
     def check_tracking_from_har(
         self, test_case: str, tracking_plan: str, har: dict
     ) -> list:
+        """Performs checks of a har dict file against a tracking plan
+
+        ``test_case`` (str): test case id (same id used to match with tracking plan)
+        ``tracking_plan`` (str): path to tracking plan file (see Documentation)
+        ``har`` (dict): actual har for this test case in dict format.
+
+        Example:
+
+        | ${checklist} =       | `Check Tracking From HAR` | ${TEST NAME} | ${tracking_plan} | ${har}
+        | `Should Not Contain` | ${checklist}              | ${False}
+        """
         r = gaunit.check_har(test_case, tracking_plan, har=har)
+        # store result in class instance for further use
+        self.result = r
+        return r.checklist_expected
+
+    def check_tracking_from_perf_log(self, test_case: str, tracking_plan: str) -> list:
+
+        perf_log = self._get_perf_log()
+        r = gaunit.check_perf_log(test_case, tracking_plan, perf_log)
+        # store result in class instance for further use
         self.result = r
         return r.checklist_expected
 
     def get_status_expected_events(self) -> list:
+
         r = self.result
         if r:
             return r.get_status_expected_events()
         else:
             return None
+
+    def _get_perf_log(self):
+
+        # get Performance Log from current webdriver instance
+        perf_log = (
+            BuiltIn()
+            .get_library_instance("SeleniumLibrary")
+            .driver.get_log("performance")
+        )
+        return perf_log
---
+++ samples/home_engie_perf_log.robot
@@ -0,0 +1,35 @@
+*** Settings ***
+Library           Collections
+Library           SeleniumLibrary
+Library           BrowserMobProxyLibrary
+Library           RequestsLibrary
+Library           GAUnitLibrary
+Suite Setup       Start Browser
+Suite Teardown    Close Browsers
+
+*** Variables ***
+# ${PAGE_URL}             https://particuliers.engie.fr?env_work=acc
+${PAGE_URL}               https://particuliers.engie.fr
+${bouton_souscription}    xpath=//*[@id='engie_fournisseur_d_electricite_et_de_gaz_naturel_headerhp_souscrire_a_une_offre_d_energie']
+${tracking_plan}          ${CURDIR}/tracking_plan.json
+
+*** Keywords ***
+Start Browser
+# Configure Webdriver to use BrowserMob Proxy
+    ${dc}                Evaluate             sys.modules['selenium.webdriver'].DesiredCapabilities.CHROME    
+    ${log_level}         Create Dictionary    performance                                                     ALL
+    Set To Dictionary    ${dc}                goog:loggingPrefs                                               ${log_level}
+    Create WebDriver     Chrome               desired_capabilities=${dc}
+
+Close Browsers
+    Close All Browsers
+
+*** Test Cases ***
+home_engie
+    set selenium implicit wait    10
+    Go To                         ${PAGE_URL}
+    Sleep                         3
+    Click Element                 ${bouton_souscription}
+    Sleep                         1
+    ${checklist} =                Check Tracking From Perf Log    ${TEST NAME}    ${tracking_plan} 
+    Should Not Contain            ${checklist}                    ${False}
